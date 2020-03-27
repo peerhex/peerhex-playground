fba3285a92031a504a81606df6c53d31fbaced7f
Jim Pick <jim@protocol.ai>
2020-03-27 12:46:25 -0700

    Add example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DeckGL from '@deck.gl/react';
+import {LineLayer} from '@deck.gl/layers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Viewport settings
+const viewState = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+};
+
+// Data to be used by the LineLayer
+const data = [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}];
+
+// DeckGL react component
+class App extends React.Component {
+  render() {
+    const layers = [
+      new LineLayer({id: 'line-layer', data})
+    ];
+
+    return (
+      <DeckGL viewState={viewState} layers={layers} />
+    );
+  }
 }
 
 export default App;
