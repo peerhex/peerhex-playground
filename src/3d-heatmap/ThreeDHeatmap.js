4aa711d8086d4362f05a08fed0d44b6732c7b7bf
Jim Pick <jim@protocol.ai>
2020-03-27 14:31:29 -0700

    Add 3d heatmap example

---
+++ src/3d-heatmap/ThreeDHeatmap.js
@@ -0,0 +1,146 @@
+import React, { Component, useState, useEffect } from 'react'
+import { StaticMap } from 'react-map-gl'
+import { AmbientLight, PointLight, LightingEffect } from '@deck.gl/core'
+import { HexagonLayer } from '@deck.gl/aggregation-layers'
+import DeckGL from '@deck.gl/react'
+import { csv } from 'd3-fetch'
+
+// Set your mapbox token here
+const MAPBOX_TOKEN = localStorage.getItem('mapbox_token')
+
+// Source data CSV
+const DATA_URL = process.env.PUBLIC_URL + '/heatmap-data.csv'
+// 'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv' // eslint-disable-line
+
+const ambientLight = new AmbientLight({
+  color: [255, 255, 255],
+  intensity: 1.0
+})
+
+const pointLight1 = new PointLight({
+  color: [255, 255, 255],
+  intensity: 0.8,
+  position: [-0.144528, 49.739968, 80000]
+})
+
+const pointLight2 = new PointLight({
+  color: [255, 255, 255],
+  intensity: 0.8,
+  position: [-3.807751, 54.104682, 8000]
+})
+
+const lightingEffect = new LightingEffect({
+  ambientLight,
+  pointLight1,
+  pointLight2
+})
+
+const material = {
+  ambient: 0.64,
+  diffuse: 0.6,
+  shininess: 32,
+  specularColor: [51, 51, 51]
+}
+
+const INITIAL_VIEW_STATE = {
+  longitude: -1.4157267858730052,
+  latitude: 52.232395363869415,
+  zoom: 6.6,
+  minZoom: 5,
+  maxZoom: 15,
+  pitch: 40.5,
+  bearing: -27.396674584323023
+}
+
+const colorRange = [
+  [1, 152, 189],
+  [73, 227, 206],
+  [216, 254, 181],
+  [254, 237, 177],
+  [254, 173, 84],
+  [209, 55, 78]
+]
+
+const elevationScale = { min: 1, max: 50 }
+
+class Heatmap extends Component {
+  static get defaultColorRange () {
+    return colorRange
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      elevationScale: elevationScale.min
+    }
+  }
+
+  _renderLayers () {
+    const {
+      data,
+      radius = 1000,
+      upperPercentile = 100,
+      coverage = 1
+    } = this.props
+
+    return [
+      new HexagonLayer({
+        id: 'heatmap',
+        colorRange,
+        coverage,
+        data,
+        elevationRange: [0, 3000],
+        elevationScale: data && data.length ? 50 : 0,
+        extruded: true,
+        getPosition: d => d,
+        onHover: this.props.onHover,
+        pickable: Boolean(this.props.onHover),
+        radius,
+        upperPercentile,
+        material,
+
+        transitions: {
+          elevationScale: 3000
+        }
+      })
+    ]
+  }
+
+  render () {
+    const { mapStyle = 'mapbox://styles/mapbox/dark-v9' } = this.props
+
+    return (
+      <DeckGL
+        layers={this._renderLayers()}
+        effects={[lightingEffect]}
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+      >
+        <StaticMap
+          reuseMaps
+          mapStyle={mapStyle}
+          preventStyleDiffing={true}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+        />
+      </DeckGL>
+    )
+  }
+}
+
+export default function ThreeDHeatmap () {
+  const [data, setData] = useState()
+  useEffect(() => {
+    console.log('Jim loading')
+    csv(DATA_URL, d => [Number(d.lng), Number(d.lat)])
+      .then(data => setData(data))
+  }, [])
+  if (!data) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div style={{ position: 'relative', height: '80vh' }}>
+      <Heatmap data={data} />
+    </div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import LineLayerExample from './line-layer/LineLayerExample'
+import ThreeDHeatmap from './3d-heatmap/ThreeDHeatmap'
 
 function Home () {
   return <h2>Home</h2>
@@ -16,13 +17,19 @@ export default function App () {
               <Link to='/'>Home</Link>
             </li>
             <li>
-              <Link to='/line-layer'>Line Layer Example</Link>
+              <Link to='/line-layer'>Line Layer</Link>
+            </li>
+            <li>
+              <Link to='/3d-heatmap'>3D Heatmap</Link>
             </li>
           </ul>
         </nav>
         <Switch>
           <Route path='/line-layer'>
             <LineLayerExample />
+          </Route>
+          <Route path='/3d-heatmap'>
+            <ThreeDHeatmap />
           </Route>
           <Route path='/'>
             <Home />
